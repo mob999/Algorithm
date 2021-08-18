ec36803eaefc3bffe5f6966556209a96297c87f0
gc <1790758006@qq.com>
2021-08-18 17:32:03 +0800

    好方法

--- lengthOfLastWord.cpp
+++ lengthOfLastWord.cpp
@@ -1,27 +1,27 @@
+/*
+给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中最后一个单词的长度。
+*/
+
 #include<string>
 #include<iostream>
 using std::string;
 using std::cout;
+
 int lengthOfLastWord(string s) {
-    if(s.empty()){
+    int len = s.size() - 1;
+    int res = 0;
+    if(len == -1)
         return 0;
+    while(len >= 0 && s[len] == ' '){
+        len--;
     }
-    int curLen = 0;
-    bool flag = false;
-    for(int i = 0; i < s.size(); i++){
-        if(s[i] != ' ' && flag == true){
-            curLen++;
-        }
-        else if(s[i] != ' ' && flag == false){
-            flag = true;
-            curLen = 1;
-        }
-        else{
-            flag = false;
-        }
+    while(len >= 0 && s[len] !=' '){
+        res++;
+        len--;
     }
-    return curLen;
+    return res;
 }
+
 
 int main(){
     string s("Today is a nice day     ");
